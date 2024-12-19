d0856c2230e37db53de64522e1506bbdf7a76ce2
jaymarketjo <2307136626@qq.com>
2024-12-19 17:13:41 +0800

    100.py

--- 100.py
+++ 100.py
@@ -2,6 +2,8 @@ import string
 import random
 import time
 
-random_list=string.ascii_letters
+#random_list=string.ascii_letters
+random=string.ascii_uppercase
 
-print(random_list)
+#print(random_list)
+print(random)
