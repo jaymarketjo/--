24b65c14535a0745fd1aaa0c3ef29e4ab5cf99fa
jaymarketjo <2307136626@qq.com>
2025-01-13 16:46:31 +0800

    python导入模块.py

--- python导入模块.py
+++ python导入模块.py
@@ -1,11 +1,8 @@
 #import math
-
 # 使用dir()查看模块中的所有功能
 #print(dir(math))
-
 # 使用help()获取特定功能的详细信息
 #help(math.sqrt)
-
 #print(math.sqrt(16))  # 使用math模块中的sqrt函数计算平方根
 
 import json
@@ -18,3 +15,4 @@ module_functions = dir(math)
 json_module_functions = json.dumps(module_functions, indent=4)
 print(json_module_functions)
 help(math.sqrt)
+print(math.sqrt(16))
